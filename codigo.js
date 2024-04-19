6fbeece62e58beab2549c382bd709604d6858c77
EzeMoreira <eezequielmoreira@gmail.com>
2024-04-18 23:48:18 -0300

    last change JS

--- codigo.js
+++ codigo.js
@@ -3,12 +3,12 @@
 //Creamos una funcion para poder arrastrar las texturas y colocarlas en la zona y que la misma renderice
 
 const zona = document.querySelector(".zona");
-zona.addEventListener("dragOver", (e)=>{
+zona.addEventListener("dragover", (e)=>{
     e.preventDefault();
-    
+});    
 //Ahora creamos un evento para cuando soltemos la textura dentro de la zona    
-})
-zona.addEventListener("drop", (e)=>{
+
+zona.addEventListener("drop",(e)=>{
     let n = e.dataTransfer.getData("textura");
     zona.style.background = `url("textura${n}.png")`;
 })
@@ -16,8 +16,8 @@ zona.addEventListener("drop", (e)=>{
 //Con preventDefault prevenimos el comportamiento por default que no nos deja soltar el elemento
 //Añadimos un evento para poder arrastrar las texturas 
 
-for (let i = 1; i < document.querySelector(".texturas").children.length + 1; i++) {
-    document.querySelector(`.textura${i}`).addEventListener("dragStart",(e)=>transferirTextura(i,e))
+for (let i = 1; i < document.querySelector(".texturas").children.length + 1; i++){
+    document.querySelector(`.textura${i}`).addEventListener("dragstart", (e) => transferirTextura(i,e))
 }
 
 const transferirTextura = (n,e) => {
